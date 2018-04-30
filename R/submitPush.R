3b69328ce0db4c360d53b4e16f6cc2e15c60e9f0
ABSOD <32849428+ABSOD@users.noreply.github.com>
2018-04-30 08:52:35 -0600

    GA Update

--- R/submitPush.R
+++ R/submitPush.R
@@ -1,4 +1,4 @@
-#' SubmitPush Writes a script tag with a dataLayer.push command  on clicking of the "View Profile" button
+#' submitPush Writes a script tag with a dataLayer.push command  on clicking of the "View Profile" button
 #'
 #' @param lvl the selected level (Counties or Municipalities)
 #' @param unit the selected location
@@ -6,55 +6,54 @@
 #' @export
 #'
 submitPush <- function(lvl,unit,topics) {
-  
-  browser()
-  lvlStr <- paste0("'level' : '",lvl,"'")
-  unitStr <-paste0("'unit' : '",unit,"'")
+
+  lvlStr <- paste0("'Level' : '",lvl,"'")
+  unitStr <-paste0("'Unit' : '",unit,"'")
   
   if("stats" %in% topics) {
-    statsStr <- "'stats' : true"
+    statsStr <- "'Basic_Stats' : 'yes'"
   }else {
-    statsStr <- "'stats' : false"
+    statsStr <- "'Basic_Stats' : 'no'"
   }  
   if("popf" %in% topics) {
-    popfStr <- "'popf' : true"
+    popfStr <- "'Pop_Forecast' : 'yes'"
   }else {
-    popfStr <- "'popf' : false"
+    popfStr <- "'Pop_Forecast' : 'no'"
   }  
   if("pop" %in% topics) {
-    popStr <- "'pop' : true"
+    popStr <- "'Pop_Age' : 'yes'"
   }else {
-    popStr <- "'pop' : false"
+    popStr <- "'Pop_Age' : 'no'"
   }
   if("popc" %in% topics) {
-    popcStr <- "'popc' : true"
+    popcStr <- "'Pop_Other' : 'yes'"
   }else {
-    popcStr <- "'popc' : false"
+    popcStr <- "'Pop_Other' : 'no'"
   }
   if("housing" %in% topics) {
-    housingStr <- "'housing' : true"
+    housingStr <- "'Housing' : 'yes'"
   }else {
-    housingStr <- "'housing' : false"
+    housingStr <- "'Housing' : 'no'"
   }  
   if("comm" %in% topics) {
-    commStr <- "'comm' : true"
+    commStr <- "'Commuting' : 'yes'"
   }else {
-    commStr <- "'comm' : false"
+    commStr <- "'Commuting' : 'no'"
   }  
   if("emplind" %in% topics) {
-    emplindStr <- "'emplind' : true"
+    emplindStr <- "'Employ_Ind' : 'yes'"
   }else {
-    emplindStr <- "'emplind' : false"
+    emplindStr <- "'Employ_Ind' : 'no'"
   } 
   if("emply" %in% topics) {
-    emplyStr <- "'emply' : true"
+    emplyStr <- "'Employ_Wage' : 'yes'"
   }else {
-    emplyStr <- "'emply' : false"
+    emplyStr <- "'Employ_Wage' : 'no'"
   }  
     
   #assembling script
-  outstr <-paste0("<script>dataLayer.push({",lvlStr,",",unitStr,",",statsStr,",",popfStr,",",
-                  popStr,",",popcStr,",",housingStr,",",commStr,",",emplindStr,",",emplyStr,", 'event':'viewProfile'})</script>")
+  outstr <-paste0("dataLayer.push({",lvlStr,",",unitStr,",",statsStr,",",popfStr,",",
+                  popStr,",",popcStr,",",housingStr,",",commStr,",",emplindStr,",",emplyStr,", 'event':'viewProfile'})")
   
   return(outstr)
 }
