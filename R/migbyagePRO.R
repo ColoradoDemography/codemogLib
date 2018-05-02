83e941a983c032f2ca9d7eff68cfbf00c621c351
ABSOD <32849428+ABSOD@users.noreply.github.com>
2018-05-02 11:41:25 -0600

    Updated Chart

--- R/migbyagePRO.R
+++ R/migbyagePRO.R
@@ -35,11 +35,11 @@ migbyagePRO <- function(listID, base=10) {
                    user = "codemog", password = pw)
   rm(pw) # removes the password
 
-  sqlPlace <- paste0("SELECT fips, county, agegroup, rate0010 FROM data.netmigrbyage WHERE fips = ",as.numeric(ctyfips),";")
+  sqlPlace <- paste0("SELECT fips, county, agegroup, netmig0010 FROM data.netmigrbyage WHERE fips = ",as.numeric(ctyfips),";")
   f.migPlace <- dbGetQuery(con, sqlPlace)
 
-  sqlState <- paste0("SELECT fips, county, agegroup, rate0010 FROM data.netmigrbyage WHERE fips = ",state,";")
-  f.migState <- dbGetQuery(con, sqlState)
+ # sqlState <- paste0("SELECT fips, county, agegroup, rate0010 FROM data.netmigrbyage WHERE fips = ",state,";")
+ # f.migState <- dbGetQuery(con, sqlState)
 
   #Closing the connection
   dbDisconnect(con)
@@ -49,25 +49,26 @@ migbyagePRO <- function(listID, base=10) {
 
   # Preparing for merge
   f.migPlace[2] <- ctyname
-  f.migState[2] <- "Colorado"
+ # f.migState[2] <- "Colorado"
 
 
-  f.migplot <- rbind(f.migPlace, f.migState)
+ # f.migplot <- rbind(f.migPlace, f.migState)
+   f.migplot <- f.migPlace
   names(f.migplot)[2] <- "geoname"
 
   f.migplot <- f.migplot[which(f.migplot$agegroup < 75),]
-  f.migplot$geoname <- factor(f.migplot$geoname, levels=c(ctyname, "Colorado"))
-  pltTitle <- "Net Migration Rate by Age: 2000-2010"
+  #f.migplot$geoname <- factor(f.migplot$geoname, levels=c(ctyname, "Colorado"))
+  pltTitle <- "Net Migration  by Age: 2000-2010"
   subTitle <- ctyname
   xTitle = "Age Group"
 
 
 
 
-  p <- f.migplot %>%ggplot(aes(x=agegroup, y=rate0010, colour=geoname))+
-    geom_line(size=1.5) +
-    scale_colour_manual("Geography", values=c("#6EC4E8", "#00953A")) +
+  p <- f.migplot %>%ggplot(aes(x=agegroup, y=netmig0010))+
+    geom_bar(stat="identity", position="dodge", fill="#6EC4E8") +
     theme_codemog(base_size=base)+
+    scale_y_continuous(labels = comma) +
     geom_hline(yintercept=0, size=1.05) +
     labs(title = pltTitle,
          subtitle = subTitle,
@@ -82,13 +83,15 @@ migbyagePRO <- function(listID, base=10) {
 
 
   #Preparing data set
-  f.migD <- merge(f.migPlace,f.migState, by="agegroup")
-  f.migData <- f.migD[,c(1,4,7)]
-  names(f.migData) <- c("5-Year Age Group",paste0("2000-2010 Migration Rate: ",ctyname), "2000-2010 Migration Rate: Colorado")
+  # f.migD <- merge(f.migPlace,f.migState, by="agegroup")
+
+  f.migD <- f.migPlace
+  f.migData <- f.migD[,c(3,4)]
+  names(f.migData) <- c("5-Year Age Group",paste0("2000-2010 Migration Rate: ",ctyname))
 
   #Geneerating Text
-   OutText <-paste0("This plot allows a comparison of net migration by age between ",ctyname," and Colorado.")  
-   OutText <-paste0(OutText," Colorado typically draws many young adults as net migrants. Areas with colleges and resorts draw a number of 18 to 24 year olds.")
+   OutText <-paste0("This plot shows the net migration by age in ",ctyname,". ")  
+   OutText <-paste0(OutText," Colorado typically draws many young adults as migrants. Areas with colleges and resorts draw a number of 18 to 24 year olds.")
    OutText <-paste0(OutText," Areas with a growing economy tend to account mostly 25 to 35 year olds and areas attractive to retirees")
    OutText <-paste0(OutText," tend to draw both workers and older adults.")
   
