59875c003e314041e3604348fc471ac3bf29469c
ABSOD <32849428+ABSOD@users.noreply.github.com>
2018-05-07 15:51:32 -0600

    Maintenance

--- R/agePlotPRO.R
+++ R/agePlotPRO.R
@@ -147,13 +147,13 @@ agePlotPRO  <- function(listID, ACS, state=0, yrs, base=10, agegroup="ten") {
   barCol <- c("#6EC4E8","#00953A")
   pltTitle <- paste0("Population Distribution by Age for ",yrs)
 
-
-  axs <- setAxis(f.AgePlot$age_Prop)
-
+  maxAxis <- round(max(f.AgePlot$age_Prop),digits=0) + 2
+  valSeq <- seq(0,maxAxis,4)
+  
   AgePlot <- f.AgePlot %>%
     ggplot(aes(x=agecat, y=age_Prop, fill=geoname))+
     geom_bar(stat="identity",color="black", position = position_dodge()) +
-    scale_y_continuous(limits=c(axs$minBrk,axs$maxBrk), breaks=axs$yBrk, label=percent, expand = c(0, 0))+
+    scale_y_continuous(limits=c(0,maxAxis),breaks=valSeq, label=percent, expand = c(0, 0))+
     scale_fill_manual(values=barCol, name="Geography") +
     theme_codemog(base_size=base)+
     theme(axis.text.x=element_text(angle=45, hjust=1))+
