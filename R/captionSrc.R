7386f9250eff63337af59f6504698085c7011272
ABSOD <32849428+ABSOD@users.noreply.github.com>
2018-05-10 08:26:19 -0600

    Maintenance

--- R/captionSrc.R
+++ R/captionSrc.R
@@ -10,22 +10,22 @@ captionSrc <- function(type, dataSrc) {
   dateStr <- paste0(", Print Date: ",as.character(format(Sys.Date(),"%m/%d/%Y")))
 
   if(type == "SDO") {
-    srcStr <- paste0("Source: State Demography Office ", dateStr)
+    srcStr <- paste0("Source: State Demography Office", dateStr)
   }
 
   if(type == "SDOBEA") {
-    srcStr <- paste0("Source: State Demography Office and U.S. Bureau of Economic Analysis ", dateStr)
+    srcStr <- paste0("Source: State Demography Office and U.S. Bureau of Economic Analysis", dateStr)
   }
   if(type == "LODES") {
-    srcStr <- paste0("Source: U.S. Census Bureau On the Map ", dateStr)
+    srcStr <- paste0("Source: U.S. Census Bureau On the Map", dateStr)
   }
   if(type == "QCEW") {
-    srcStr <- paste0("Source: Department of Labor and Employment (QCEW) ", dateStr)
+    srcStr <- paste0("Source: Department of Labor and Employment (QCEW)", dateStr)
   }
   if(type =="ACS") {
     byr <- paste0("20",substr(dataSrc,4,5))
     eyr <- paste0("20",substr(dataSrc,6,7))
-    srcStr <- paste0("Source: U.S. Census Bureau, ",byr,"-",eyr," American Community Survey. ", dateStr)
+    srcStr <- paste0("Source: U.S. Census Bureau, ",byr,"-",eyr," American Community Survey", dateStr)
   }
   return(srcStr)
 }
